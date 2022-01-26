1d6f3aa07e131e46cbfe3055c6879e59121afac8
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2022-01-25 20:10:41 -0500

    Creando estructura inicial para componente App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-
+import React, { useState } from "react";
+import text from "./data";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world');
+  }
+
   return (
-    <div className="App">
-      <h2>Hello world</h2>
-    </div>
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">
+          Paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)} />
+
+        <button type="submit" className="btn">Generate text</button>
+      </form>
+
+      <arcile className="lorem-text">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, mollitia! Ipsam enim incidunt quam aspernatur eveniet ut soluta optio! Aspernatur exercitationem dolorem sed illum minima temporibus, libero laboriosam recusandae reiciendis!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, mollitia! Ipsam enim incidunt quam aspernatur eveniet ut soluta optio! Aspernatur exercitationem dolorem sed illum minima temporibus, libero laboriosam recusandae reiciendis!</p>
+      </arcile>
+    </section>
   );
 }
 
